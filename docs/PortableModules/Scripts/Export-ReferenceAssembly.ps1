fdeb4626193e828a55547b9f5403bcaf60adffc4
James Truher <jimtru@microsoft.com>
2017-12-07 16:59:17 -0800

    Add Interfaces and Structs

--- docs/PortableModules/Scripts/Export-ReferenceAssembly.ps1
+++ docs/PortableModules/Scripts/Export-ReferenceAssembly.ps1
@@ -7,6 +7,19 @@ function Get-Exclusions
     $script:ExcludedItems = get-content $exclusionFile | ConvertFrom-Json
 }
 
+function Fix-Name ( $name ) 
+{
+    $cSharpKeywords = "abstract","as","base","bool","break","byte","case","catch","char","checked","class","const",
+        "continue","decimal","default","delegate","do","double","else","enum","event","explicit","extern","false",
+        "finally","fixed","float","for","foreach","goto","if","implicit","in","int","interface","internal","is",
+        "lock","long","namespace","new","null","object","operator","out","override","params","private","protected",
+        "public","readonly","ref","return","sbyte","sealed","short","sizeof","stackalloc","static","string","struct",
+        "switch","this","throw","true","try","typeof","uint","ulong","unchecked","unsafe","ushort","using","virtual",
+        "void","volatile","while"
+    if ( $cSharpKeywords -contains $name ) { return "@${name}" } else { return "${name}" }
+}
+
+
 function Exclude-Member ( $name ) {
     #$script:namespace
     #$script:CurrentClass
@@ -26,6 +39,35 @@ function Exclude-Class ( $name ) {
     return $false
 }
 
+function Get-tType ( $tType )
+{
+    $p = @()
+    if ( $tType.IsGenericType ) {
+        $pp = $tType.FullName
+        if ( ! $pp ) { $pp = $tType.Namespace + "." + $tType.Name }
+        $pp = Get-TypeNickname ( $pp -replace "``.*" )
+        $gp = @()
+        $argsAndParms = @(); $argsAndParms += $tType.GenericTypeArguments; $argsAndParms += $tType.GenericTypeParameters
+        foreach( $pt in $argsAndParms ) { # $tType.GenericTypeArguments) {
+            if ( $pt.IsGenericType )
+            {
+                $gp += Get-tType $pt
+            }
+            else {
+                $aa = $pt.FullName
+                if ( ! $aa ) { $aa = $pt.Name }
+                $gp += Get-TypeNickname $aa
+            }
+        }
+        $ss = $gp -join ", "
+        if ( $ss -eq "" ) { wait-debugger }
+        $p += "{0}<{1}>" -f $pp, $ss
+    }
+    else {
+        $p += Get-TypeNickname $tType.FullName
+    }
+    $p -join ", "
+}
 
 
 function GetTypeName ( $type )
@@ -48,22 +90,34 @@ function GetTypeName ( $type )
 function Get-TypeNickname ( [string]$typename )
 {
     $TypeTranslation = @{
-        "Object" = "object"
-        "Boolean" = "bool"
-        "UInt32" = "uint"
-        "String" = "string"
-        "Int32" = "int"
-        "Void" = "void"
-
-        "Object[]" = "object[]"
+        "Object"          = "object"
+        "System.Object"   = "object"
+        "Object[]"        = "object[]"
         "System.Object[]" = "object[]"
+       
+        "Boolean"         = "bool"
+        "System.Boolean"  = "bool"
 
-        "System.Object" = "object"
-        "System.Boolean" = "bool"
-        "System.UInt32" = "uint"
-        "System.String" = "string"
-        "System.Int32" = "int"
-        "System.Void" = "void"
+        "String[]"        = "string[]"
+        "System.String[]" = "string[]"
+        "String"          = "string"
+        "System.String"   = "string"
+
+        "Int32"           = "int"
+        "System.Int32"    = "int"
+        "UInt32"          = "uint"
+        "System.UInt32"   = "uint"
+
+        "Void"            = "void"
+        "System.Void"     = "void"
+
+        "System.Char"     = "char"
+        "Char"            = "char"
+
+        "System.Char[]"   = "char[]"
+        "Char[]"          = "char[]"
+
+
     }
 
     $typename = $typename -replace "\+","."
@@ -90,7 +144,10 @@ function GetTypeType ( $type )
 {
     if ( $type.IsInterface ) { return "interface" }
     if ( $type.IsClass ) { return "class" }
-    if ( $type.IsEnum ) { return "enum" }
+    if ( $type.IsEnum -and $type.IsValueType ) { return "enum" }
+    if ( $type.IsValueType -and ! $type.IsEnum ) { return "struct" }
+    Write-Warning ($type.Name + " is unknown")
+    return "unknown"
 }
 
 function EmitEnum ( $t )
@@ -112,8 +169,14 @@ function EmitConstants ( $t )
     foreach ( $constant in $constants )
     {
         if ( $t.IsAbstract -and ! $t.IsSealed ) { $visibility = "protected" } else { $visibility = "public" }
-        $fmt = '    {0} const {1} {2} = "{3}";' 
-        $fmt -f $visibility,(Get-TypeNickname $constant.FieldType),$constant.Name,$constant.GetValue($null)
+        $fieldType = Get-TypeNickname $constant.FieldType
+        $fieldName = $constant.Name
+        $fieldValue= $constant.GetValue($null)
+        $multiline = ""
+        if ( $fieldValue -match "`n" ) { $multiline = "@" }
+        $fmt = '    {0} const {1} {2} = {3}"{4}";' 
+        $fmt -f $visibility,$fieldType,$fieldName,$multiline,$fieldValue
+        #$fmt -f $visibility,(Get-TypeNickname $constant.FieldType),$constant.Name,$constant.GetValue($null)
         #$const += "const "
         #$const += Get-TypeNickname $constant.FieldType
         #$const += " "
@@ -123,6 +186,25 @@ function EmitConstants ( $t )
         #$const
     }
 }
+
+function EmitAttribute ( $t )
+{
+    if ( ! $t.CustomAttributes ) { return }
+    foreach ( $attribute in $t.CustomAttributes )
+    {
+        $at = Get-tType $attribute.AttributeType
+        $s = "    [" + (get-TypeNickname $at)
+        if ( $attribute.ConstructorArguments -or $attribute.NamedArguments ) { $s += "(" }
+        $aArgs = @()
+        $aArgs += $attribute.ConstructorArguments
+        $aArgs += $attribute.NamedArguments
+        $s += $aArgs -join ", "
+        if ( $attribute.ConstructorArguments -or $attribute.NamedArguments ) { $s += ")" }
+        $s += "]"
+        $s
+    }
+}
+
 function EmitProperties ( $t )
 {
     $properties = $t.GetProperties("Static,Instance,Public,DeclaredOnly")| sort-object Name
@@ -132,28 +214,40 @@ function EmitProperties ( $t )
         # emit the member only if not excluded
         if ( Exclude-Member $property.name ) { $propertyCount--; continue }
         # check for generic
-        if ( $Property.PropertyType.IsGenericType ) {
-            #wait-debugger
-            $propertyArgs = $property.propertytype.GenericTypeArguments | %{ $a = @()}{$a += Get-TypeNickName $_.fullname}{$a -join ","}
-            $propertyType = $property.propertytype.fullname -replace "``.*"
-            $propertyString = "{0}<{1}>" -f $propertyType,$propertyArgs
-        }
-        else {
-            $propertyString = Get-TypeNickName $property.propertytype.fullname
-        }
+        $propertyString = Get-tType $property.propertytype
+        #if ( $Property.PropertyType.IsGenericType ) {
+        #    #wait-debugger
+        #    $propertyArgs = $property.propertytype.GenericTypeArguments | %{ $a = @()}{$a += Get-TypeNickName $_.fullname}{$a -join ","}
+        #    $propertyType = $property.propertytype.fullname -replace "``.*"
+        #    $propertyString = "{0}<{1}>" -f $propertyType,$propertyArgs
+        #}
+        #else {
+        #    $propertyString = Get-TypeNickName $property.propertytype.fullname
+        #}
         [string]$dec = ""
         # Attributes
         if ( $property.CustomAttributes ) {
-            # wait-debugger
             $attributes = $property.CustomAttributes | Sort-Object AttributeType
             foreach($attribute in $attributes) {
-                $dec += "    ${attribute}`n" -replace "\(\)","" -creplace " String"," string" -creplace " True","true" -creplace " False","false"
+                if ( ! $attribute.ConstructorArguments ) {
+                    $dec += "    [" + (Get-TypeNickname $attribute.AttributeType.FullName) + "]"
+                    # if ( $attribute.AttributeType.Fullname -match "\+" ) { wait-debugger }
+                }
+                else {
+                    $dec += "    ${attribute}`n" # -replace "\(\)","" -creplace " String"," string" -creplace " True","true" -creplace " False","false"
+                }
+                # if ( "$attribute" -match "\+" ) { wait-debugger }
             }
         }
-        $dec += "    public {0} {1} {{" -f $propertyString,$property.name
-        if ( $property.GetMethod ) { $dec += " get { return default($propertyString); }" }
-        if ( $property.SetMethod ) { $dec += " set { }" }
-        $dec += " }" 
+        if ( $t.IsInterface ) {
+            "    {0} {1} {{ get; }}" -f $propertyString,$property.name
+        }
+        else {
+            $dec += "    public {0} {1} {{" -f $propertyString,$property.name
+            if ( $property.GetMethod ) { $dec += " get { return default($propertyString); }" }
+            if ( $property.SetMethod ) { $dec += " set { }" }
+            $dec += " }" 
+        }
         $dec
     }
 }
@@ -201,6 +295,12 @@ function GetParams ( $method )
 {
     $parm = @()
     foreach($p in $method.GetParameters()) {
+        $pType = Get-tType $p.parametertype
+        if ( $pType -match "&$" ) { $pType = $pType.TrimEnd("&") ; $isOut = "out " } else { $isOut = "" }
+        $parm += "${isout}{0} {1}" -f $pType,(Fix-Name $p.Name)
+
+    # JWT
+        <#
         $paramType = $p.parametertype.FullName
         if ( ! $paramType ) { $paramType = $p.parametertype.Name }
         $pTypeName = Get-TypeNickname $paramType
@@ -210,18 +310,20 @@ function GetParams ( $method )
             $pTypeName += "<" + ($p.ParameterType.GenericTypeArguments -join ",") + ">"
         }
         $parm += "${isout}{0} {1}" -f $pTypeName,$p.name
+        #>
     }
     return ($parm -join ", ")
 }
 
-function GetDefaultReturn ( $method ) {
+function GetDefaultReturn ( $method, $rt ) {
     if ( $method.ReturnType.Name -ne "void" ) {
-        if ( $method.ReturnType.IsGenericType ) {
-            "return default(" + ($method.ReturnType.ToString() -replace "``.*") + ");" 
-        }
-        else {
-            "return default(" + ( Get-TypeNickname $method.ReturnType.FullName ) + ");"
-        }
+        "return default($rt);"
+        #if ( $method.ReturnType.IsGenericType ) {
+        #    "return default(" + ($method.ReturnType.ToString() -replace "``.*") + ");" 
+        #}
+        #else {
+        #    "return default(" + ( Get-TypeNickname $method.ReturnType.FullName ) + ");"
+        #}
     }
 }
 
@@ -229,14 +331,40 @@ function Get-NestedTypes ( $t ) {
     $t.GetNestedTypes("Public,Instance,Static")
 }
 
+function Get-MethodNameAndReturnType ( $method, [ref] $returnT )
+{
+    $name = $method.name
+    #$returnType = Get-TypeNickname (GetTypeName $method.ReturnType)
+    $returnType = Get-TypeNickname (Get-tType $method.ReturnType)
+    $returnT.Value = $returnType
+    switch ( $name ) {
+        "op_Equality"           { "${returnType} operator =="; break }
+        "op_GreaterThan"        { "${returnType} operator >"; break }
+        "op_GreaterThanOrEqual" { "${returnType} operator >="; break }
+        "op_Inequality"         { "${returnType} operator !="; break }
+        "op_LessThan"           { "${returnType} operator <"; break }
+        "op_LessThanOrEqual"    { "${returnType} operator <="; break }
+
+        "op_Explicit"           { "explicit operator ${returnType}"; break }
+        "op_Implicit"           { "implicit operator ${returnType}"; break }
+        default { 
+            "${returnType} ${name}" 
+            }
+    }
+}
+
 function EmitMethods ( $t )
 {
     $methods = $t.GetMethods("Instance,Static,NonPublic,Public,DeclaredOnly") | sort-object name
     $methodCount = $methods.Count
+    $IsInterface = $t.IsInterface
     foreach ( $method in $methods ) {
         if ( $method.name -cmatch "^[gs]et_|^add_|^remove_" ) { $methodCount--; continue }
         $sig = @()
-        if ( $method.IsFamilyOrAssembly ) {
+        if ( $IsInterface ) {
+            $sig += "    "
+        }
+        elseif ( $method.IsFamilyOrAssembly ) {
             $sig += "    protected internal"
         }
         elseif ( $method.IsPublic ) {
@@ -249,7 +377,7 @@ function EmitMethods ( $t )
             $methodCount--
             continue; # don't emit
         }
-        if ( $method.IsVirtual ) { 
+        if ( $method.IsVirtual -and ! $t.IsInterface ) { 
             if ( $t.IsAbstract ) {
                 $sig += "virtual" 
             }
@@ -258,12 +386,30 @@ function EmitMethods ( $t )
             }
         }
         if ( $method.IsStatic ) { $sig += "static" }
-        $sig += Get-TypeNickname (GetTypeName $method.ReturnType) # .FullName
-        $sig += $method.Name + "("
+        $rt = $null
+        $mrt = Get-MethodNameAndReturnType $method ([ref]$rt)
+        #if ( $mrt -match "IEnum.*Invoke") { wait-debugger }
+        #if ( $mrt.length -eq 0 ) { wait-debugger }
+        $sig += $mrt
+        #if ( $method.name -match "^op_" ) {
+        #    $sig += (Get-MethodName $method.name)
+        #    $sig += Get-TypeNickname (GetTypeName $method.ReturnType) # .FullName
+        #}
+        #else {
+        #    $sig += Get-TypeNickname (GetTypeName $method.ReturnType) # .FullName
+        #    $sig += $method.name # (Get-MethodName $method.name)
+        #}
+        $sig += "("
+        #$sig += $method.Name + "("
         $sig += GetParams $method
-        $sig += ") {"
-        $sig += GetDefaultReturn $method
-        $sig += "}"
+        if ( $IsInterface ) {
+            $sig += ");"
+        }
+        else {
+            $sig += ") {"
+            $sig += GetDefaultReturn $method $rt
+            $sig += "}"
+        }
         $sig -join " "
     }
     if ( $methodCount -gt 0 ) { "" }
@@ -298,9 +444,22 @@ function GetBaseType ( $type )
 {
     $BaseType = $type.BaseType.FullName
     if ( $type.isClass -and $type.isgenerictypedefinition ) { 
-        $ttype = $type.GetGenericArguments().Name
-
-        return "where ${ttype} : class" 
+        $s = ""
+        if ( $type.ImplementedInterfaces ) {
+            $iface = $type.ImplementedInterfaces | %{ Get-TypeNickName $_ }
+            $s += ": " + ($iface -join ", ")
+        }
+        $ga = $type.GetGenericArguments()
+        if ( $ga ) {
+            $s += "where {0} : {1}" -f ($ga.Name -join ", "),(($ga.BaseType|%{"$_" -replace "System.ValueType","struct, System.IConvertible"}) -join ", ")
+        }
+        return $s
+        #$ttype = $type.GetGenericArguments().Name
+        #$constraints = @()
+        #if ( $type.StructLayoutAttribute ) { $constraints += "struct" }
+        #$contraints += $type.ImplementedInterfaces | %{ Get-TypeNickname $_ }
+        #$constraints += $type.GenericTypeParameters.ImplementedInterfaces | %{ Get-TypeNickname $_ }
+        # return ("where ${ttype} : {0}" -f ($constraints -join ", "))
     }
     switch ( $BaseType ) {
         "System.Object" { return }
@@ -317,6 +476,10 @@ function Get-TypeAttribute
     $attrs = $t.CustomAttributes
     foreach ( $attr in $attrs )
     {
+        if ( @($attr.ConstructorArguments).Length -eq 0 -and @($attr.NamedArguments).Length -eq 0 ) {
+            "[{0}]`n" -f (Get-TypeNickname ($attr.AttributeType))
+            continue
+        }
         #switch ( $attr.TypeId )
         switch ( $attr.AttributeType.FullName )
         {
@@ -366,7 +529,7 @@ function Get-TypeAttribute
             "System.Xml.Serialization.XmlTypeAttribute" {
                 "   ${attr}`n"
             }
-            default: {
+            default {
                 "DEFAULT ${attr}`n"
             }
         }
@@ -375,8 +538,30 @@ function Get-TypeAttribute
 
 
 
-Get-Exclusions $ExclusionFile
 
+function EmitInterface ( $t )
+{
+    "// INTERFACE: $t"
+    "  public partial interface " + $t.Name + " {"
+    EmitProperties $t
+    EmitMethods $t
+    "  }"
+}
+
+function EmitStruct ( $t )
+{
+    # constructor
+    # properties
+    # methods
+    "// STRUCT"
+    "  public partial struct " + $t.Name + " {"
+    EmitConstructors $t
+    EmitProperties $t
+    EmitMethods $t
+    "  }"
+}
+
+Get-Exclusions $ExclusionFile
 
 $Types = $assembly.GetTypes() |?{$_.IsPublic}
 $NamespaceTypes = $Types | Group-Object Namespace 
@@ -388,7 +573,7 @@ foreach ( $g in $NamespaceTypes ) {
     "namespace {0} {{" -f $g.Name
     foreach ( $t in $g.Group | sort-object Name) {
         Write-Progress -Parent 0 -Id 1 $t
-        $typeName = GetTypeName $t # which includes generics
+        $typeName = (GetTypeName $t) -replace "^.*\." # which includes generics
         $dec = @()
         $dec += Get-TypeAttribute $t
         $dec += "  public"
@@ -409,13 +594,19 @@ foreach ( $g in $NamespaceTypes ) {
                 break
             }
             # handle interface!!!!
+            "interface" {
+                EmitInterface $t
+            }
+            "struct" {
+                EmitStruct $t
+            }
             "class" {
                 $script:CurrentClass = $typeName
                 if ( Exclude-Class $typeName ) { continue }
                 #if ( $typeName -eq "VerbsOther" ) { wait-debugger }
                 $dec -join " "
-                EmitConstants $t
                 EmitConstructors $t
+                EmitConstants $t
                 EmitEvents $t
                 EmitProperties $t
                 EmitMethods $t
@@ -423,6 +614,7 @@ foreach ( $g in $NamespaceTypes ) {
                 break
             }
             default {
+                Write-Error "Unknown $t"
                 break
             }
         }
