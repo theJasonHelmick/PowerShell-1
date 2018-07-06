e35ce65ef0f19a4d56f10edac99e8f5c59397136
James Truher <jimtru@microsoft.com>
2018-07-06 14:16:58 -0700

    Modify way tests are skipped or marked pending to use splatting

--- test/powershell/Modules/Microsoft.PowerShell.Management/Clear-EventLog.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Clear-EventLog.Tests.ps1
@@ -3,15 +3,15 @@
 Describe "Clear-EventLog cmdlet tests" -Tags @('CI', 'RequireAdminOnWindows') {
 
     BeforeAll {
-        $defaultParamValues = $PSdefaultParameterValues.Clone()
-        $PSDefaultParameterValues["it:skip"] = !$IsWindows -or $IsCoreCLR
+        if ( $IsWindows ) {
+            $SkipOrPending = @{ Pending = $true }
+        }
+        else {
+            $SkipOrPending = @{ Skip = $true }
+        }
     }
 
-    AfterAll {
-        $global:PSDefaultParameterValues = $defaultParamValues
-    }
-
-    It "should be able to Clear-EventLog" -Pending:($True) {
+    It "should be able to Clear-EventLog" @SkipOrPending {
       Remove-EventLog -LogName TestLog -ErrorAction Ignore
       { New-EventLog -LogName TestLog -Source TestSource -ErrorAction Stop } | Should -Not -Throw
       { Write-EventLog -LogName TestLog -Source TestSource -Message "Test" -EventID 1 -ErrorAction Stop } | Should -Not -Throw
@@ -24,11 +24,11 @@ Describe "Clear-EventLog cmdlet tests" -Tags @('CI', 'RequireAdminOnWindows') {
       { Remove-EventLog -LogName TestLog -ErrorAction Stop }      | Should -Not -Throw
     }
 
-    It "should throw 'System.InvalidOperationException' when asked to clear a log that does not exist" -Pending:($True) {
+    It "should throw 'System.InvalidOperationException' when asked to clear a log that does not exist" @SkipOrPending {
       { Clear-EventLog -LogName MissingTestLog -ErrorAction Stop } | Should -Throw -ExceptionType "System.InvalidOperationException"
     }
 
-    It "should throw 'Microsoft.PowerShell.Commands.ClearEventLogCommand' ErrorId when asked to clear a log that does not exist" -Pending:($True) {
+    It "should throw 'Microsoft.PowerShell.Commands.ClearEventLogCommand' ErrorId when asked to clear a log that does not exist" @SkipOrPending {
       { Clear-EventLog -LogName MissingTestLog -ErrorAction Stop } | Should -Throw -ErrorId "Microsoft.PowerShell.Commands.ClearEventLogCommand"
     }
 }
